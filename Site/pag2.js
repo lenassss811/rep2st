7363bb618430417591a50bc1e751aa3a787ede6e
lorennasantosdasilva23 <lorennasantosdasilva23@gmail.com>
2025-04-29 17:20:04 +0000

    savepages

---
+++ Site/pag2.js
@@ -0,0 +1,15 @@
+function scrollLeft() {
+    document.getElementById("carousel").scrollBy({ left: -300, behavior: 'smooth' });
+}
+
+function scrollRight() {
+    document.getElementById("carousel").scrollBy({ left: 300, behavior: 'smooth' });
+}
+
+function toggleDescription(card) {
+    document.querySelectorAll('.card').forEach(c => {
+        if (c !== card) c.classList.remove('active');
+    });
+    card.classList.toggle('active');
+}
+    
